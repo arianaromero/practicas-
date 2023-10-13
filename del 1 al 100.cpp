69db30da2589bb099fc357f9ef2bcc0112751773
arianaromero <144074781+arianaromero@users.noreply.github.com>
2023-10-12 19:37:05 -0600

    codigos
    códigos del semestre

---
+++ del 1 al 100.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+        0|.0
+main() {
+    int numero;
+    cout<<"por favor digite el numero de multiplicaciom : "<<endl;
+    cin>>numero;
+    
+      for( int i =1;i <=10;i ++) {
+      	int multiplicacion = i * numero;
+      	cout<<numero <<"  x  " << i << multiplicacion <<endl;
+      	
+	  }
+    return 0; 
+}
+ 
+
---
+++ hola_mundo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"hola mundo programadores";
+	cin.get();
+	return 0;
+}
+
+
+
+
+
